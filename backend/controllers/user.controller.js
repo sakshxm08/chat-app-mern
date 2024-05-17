678c09c3743b733e6b71ecabf9822376fabf0e85
Saksham Gambhir <saksham2211gambhir@gmail.com>
2024-05-18 02:18:54 +0530

    individual user details fetch api

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -15,3 +15,16 @@ export const get_contacts = async (req, res) => {
     return res.status(501).json({ message: "Internal Server Error" });
   }
 };
+
+export const get_contact_details = async (req, res) => {
+  try {
+    const { contact_id } = req.params;
+    const contact = await User.findById(contact_id).select("-password");
+    if (!contact) return res.status(404).json({ message: "User not found" });
+    return res.status(200).json(contact);
+  } catch (error) {
+    // Log and return internal server error response
+    console.log("Error in Get Individual Contact Controller: " + error.message);
+    return res.status(501).json({ message: "Internal Server Error" });
+  }
+};
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { verify_token } from "../middlewares/verify_token.js";
-import { get_contacts } from "../controllers/user.controller.js";
+import {
+  get_contact_details,
+  get_contacts,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/", verify_token, get_contacts);
+router.get("/:contact_id", verify_token, get_contact_details);
 
 export default router;
