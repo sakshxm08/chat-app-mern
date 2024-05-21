3cc563a836b63e13bf113a4afe45ee6c7cd45045
Saksham Gambhir <saksham2211gambhir@gmail.com>
2024-05-21 16:21:18 +0530

    socket.io url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,17 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  "/api": {
-    target: "http://localhost:3000", // Assuming backend runs on port 3000
-    changeOrigin: true,
-    secure: false,
-  },
-  "/socket.io": {
-    target: "http://localhost:3000", // Proxy socket.io requests
-    changeOrigin: true,
-    ws: true, // Enable WebSocket proxying
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000", // Assuming backend runs on port 3000
+        changeOrigin: true,
+        secure: false,
+      },
+      "/socket.io": {
+        target: "http://localhost:3000", // Proxy socket.io requests
+        ws: true, // Enable WebSocket proxying
+      },
+    },
   },
 });
