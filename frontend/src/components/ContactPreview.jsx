6c19ca626f2768b108f2e2b838e3a3987a9c25cd
Saksham Gambhir <saksham2211gambhir@gmail.com>
2024-05-22 11:35:28 +0530

    responsive for medium devices

--- frontend/src/components/ContactPreview.jsx
+++ frontend/src/components/ContactPreview.jsx
@@ -9,21 +9,21 @@ const ContactPreview = ({ contact }) => {
   return (
     <Link
       to={`/m/${contact._id}`}
-      className="px-6 py-3 hover:bg-base-200 dark:hover:bg-slate-900/40 dark:text-gray-100 transition-all flex gap-4 items-center cursor-pointer"
+      className="px-3 xl:px-6 py-3 hover:bg-base-200 dark:hover:bg-slate-900/40 dark:text-gray-100 transition-all flex gap-4 items-center cursor-pointer"
     >
       <div className={`avatar ${isOnline ? "online" : ""}`}>
-        <div className="w-12 rounded-full">
+        <div className="md:w-8 lg:w-10 xl:w-12 rounded-full">
           <img src={contact.avatar} />
         </div>
       </div>
-      <div className="flex flex-col w-ful">
+      <div className="flex flex-col md:w-[calc(100%-3rem)] lg:w-[calc(100%-3.5rem)] xl:w-[calc(100%-4rem)]">
         <div className="flex justify-between items-center">
-          <h4 className="font-normal">
+          <h4 className="font-normal md:text-sm lg:text-base">
             {contact.f_name} {contact.l_name}
           </h4>
-          <span className="text-xs ">2:30</span>
+          <span className="text-[10px] xl:text-xs ">2:30</span>
         </div>
-        <p className="truncate overflow-hidden text-sm text-gray-400 dark:text-gray-500 max-w-56">
+        <p className="truncate text-sm text-gray-400 dark:text-gray-500 ">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur
           minus earum modi magni natus pariatur, quidem et quaerat alias
           blanditiis.
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,10 +5,10 @@ import Sidebar from "../components/Sidebar";
 const Home = () => {
   return (
     <div className="flex w-full gap-4">
-      <div className="w-1/4 h-full">
+      <div className="w-1/3 lg:w-1/4 h-full">
         <Sidebar />
       </div>
-      <div className="w-3/4 h-full">
+      <div className="w-2/3 lg:w-3/4 h-full">
         {/* <MessageSection /> */}
         <Outlet />
       </div>
